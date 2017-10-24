cf1f83a27a5c63a90c0115a39dd516d48228eaa3
TuringMac <turingmac@hotmail.com>
2017-10-23 22:17:28 -0400

    Batch download given date & site

--- Read&Display.py
+++ Read&Display.py
@@ -5,23 +5,13 @@ import pyart.graph
 import tempfile
 import pyart.io
 import boto
+from xml.dom import minidom
+from urllib import urlopen
 
-# read a volume scan file on S3. I happen to know this file exists.
-s3conn = boto.connect_s3()
-bucket = s3conn.get_bucket('noaa-nexrad-level2')
-s3key = bucket.get_key('2015/05/15/KVWX/KVWX20150515_080737_V06.gz')
-print s3key
+date = "2016/01/01"
+site = "KGSP"
 
-# download to a local file, and read it
-localfile = tempfile.NamedTemporaryFile()
-s3key.get_contents_to_filename(localfile.name)
-radar = pyart.io.read_nexrad_archive(localfile.name)
-
-# display the lowest elevation scan data
-display = pyart.graph.RadarDisplay(radar)
-#fig = plt.figure(figsize=(9, 12))
-
-plots = [
+plotsAll = [
     # variable-name in pyart, display-name that we want, sweep-number of radar (0=lowest ref, 1=lowest velocity)
     ['reflectivity', 'Reflectivity (dBZ)', 0],
     ['differential_reflectivity', 'Zdr (dB)', 0],
@@ -31,8 +21,59 @@ plots = [
     ['spectrum_width', 'Spectrum Width', 1]
 ]
 
+reflectivityPlots = [
+        # variable-name in pyart, display-name that we want, sweep-number of radar (0=lowest ref, 1=lowest velocity)
+        ['reflectivity', 'Reflectivity (dBZ)', 0],
+        ['reflectivityqc', 'QCed Reflectivity (dBZ)', 0]
+    ]
 
-def plot_radar_images(plots):
+
+def getListText(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+
+def getDayFiles():
+    bucketURL = "http://noaa-nexrad-level2.s3.amazonaws.com"
+    dirListURL = bucketURL + "/?prefix=" + date + "/" + site
+
+    print "listing files from %s" % dirListURL
+
+    xmldoc = minidom.parse(urlopen(dirListURL))
+    itemlist = xmldoc.getElementsByTagName('Key')
+    print len(itemlist), "keys found..."
+
+    # For this test, WCT is downloaded and unzipped directly in the working directory
+    # The output files are going in 'output'
+    # http://www.ncdc.noaa.gov/wct/install.php
+    files = []
+    for x in itemlist:
+        file = getListText(x.childNodes)
+        files.append(file)
+
+    return files
+
+
+def setAWSConnection(file):
+    # read a volume scan file on S3. I happen to know this file exists.
+    s3conn = boto.connect_s3()
+    bucket = s3conn.get_bucket('noaa-nexrad-level2')
+    s3key = bucket.get_key(file)
+    print s3key
+
+    # download to a local file, and read it
+    localfile = tempfile.NamedTemporaryFile()
+    s3key.get_contents_to_filename(localfile.name)
+    radar = pyart.io.read_nexrad_archive(localfile.name)
+
+    return radar
+
+
+def gen_radar_images(display, fig, radar, plots):
+    # display the lowest elevation scan data
     ncols = 2
     nrows = len(plots) / 2
     for plotno, plot in enumerate(plots, start=1):
@@ -47,33 +88,37 @@ def plot_radar_images(plots):
     plt.show()
 
 
-#plot_radar_images(plots)
+def plot_radar_images(radar, plots):
+    refl_grid = radar.get_field(0, 'reflectivity')
+    #print refl_grid[0]
+    rhohv_grid = radar.get_field(0, 'cross_correlation_ratio')
+    zdr_grid = radar.get_field(0, 'differential_reflectivity')
 
-refl_grid = radar.get_field(0, 'reflectivity')
-#print refl_grid[0]
-rhohv_grid = radar.get_field(0, 'cross_correlation_ratio')
-zdr_grid = radar.get_field(0, 'differential_reflectivity')
+    # apply rudimentary quality control
+    reflow = np.less(refl_grid, 20)
+    zdrhigh = np.greater(np.abs(zdr_grid), 2.3)
+    rhohvlow = np.less(rhohv_grid, 0.95)
+    notweather = np.logical_or(reflow, np.logical_or(zdrhigh, rhohvlow))
+    #print notweather[0]
 
-# apply rudimentary quality control
-reflow = np.less(refl_grid, 20)
-zdrhigh = np.greater(np.abs(zdr_grid), 2.3)
-rhohvlow = np.less(rhohv_grid, 0.95)
-notweather = np.logical_or(reflow, np.logical_or(zdrhigh, rhohvlow))
-#print notweather[0]
+    qcrefl_grid = ma.masked_where(notweather, refl_grid)
+    #print qcrefl_grid[0]
 
-qcrefl_grid = ma.masked_where(notweather, refl_grid)
-#print qcrefl_grid[0]
+    # let's create a new object containing only sweep=0 so we can add the QC'ed ref to it for plotting
+    qced = radar.extract_sweeps([0])
+    qced.add_field_like('reflectivity', 'reflectivityqc', qcrefl_grid)
+    display = pyart.graph.RadarDisplay(qced)
+    fig = plt.figure(figsize=(11, 5))
 
-# let's create a new object containing only sweep=0 so we can add the QC'ed ref to it for plotting
-qced = radar.extract_sweeps([0])
-qced.add_field_like('reflectivity', 'reflectivityqc', qcrefl_grid)
-display = pyart.graph.RadarDisplay(qced)
-fig = plt.figure(figsize=(11, 5))
+    gen_radar_images(display, fig, radar, plots)
 
-plots = [
-    # variable-name in pyart, display-name that we want, sweep-number of radar (0=lowest ref, 1=lowest velocity)
-    ['reflectivity', 'Reflectivity (dBZ)', 0],
-    ['reflectivityqc', 'QCed Reflectivity (dBZ)', 0],
-]
-plot_radar_images(plots)
 
+def main():
+    files = getDayFiles()
+    for file in files:
+        radar = setAWSConnection(file)
+        #gen_radar_images(radar, plotsAll)
+        plot_radar_images(radar, reflectivityPlots)
+
+if __name__ == "__main__":
+    main()
